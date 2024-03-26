a0a287110fe69c75e80ba8aa8e96a4d349a9a3c0
Travis Cook <justuscooks@gmail.com>
2024-03-25 21:49:11 -0700

    updated axis labels

--- static/js/app.js
+++ static/js/app.js
@@ -81,6 +81,8 @@ function bubble_chart(subject_id, sample_values, otu_ids, otu_labels) {
 
     let layout = {
         title: "<b>Bacteria Cultures Per Subject ID</b>",
+        xaxis: {title: "OTU ID", titlefont: {size: 11, color: "#A0A0A0"}},
+        yaxis: {title: "Sample Values", titlefont: {size: 11, color: "#A0A0A0"}},
     };
 
     Plotly.newPlot("bubble", trace_data, layout);
